5450c0ac9fe787f9882ef2e600cc64af5f8e4044
Tomofumi Tanaka <tanacasino@gmail.com>
2015-08-05 00:27:09 +0900

    Add all static method to StaticOffsetDateTime

--- src/main/scala/com/github/tanacasino/time4s/wrappers/objects/StaticOffsetDateTime.scala
+++ src/main/scala/com/github/tanacasino/time4s/wrappers/objects/StaticOffsetDateTime.scala
@@ -11,10 +11,27 @@ object StaticOffsetDateTime extends StaticOffsetDateTime
 
 trait StaticOffsetDateTime {
 
+  def from(temporal: TemporalAccessor) = OffsetDateTime.from(temporal)
+
   def now: OffsetDateTime = OffsetDateTime.now
 
   def now(zone: ZoneId) = OffsetDateTime.now(zone)
 
   def now(clock: Clock) = OffsetDateTime.now(clock)
+
+  def of(year: Int, month: Int, dayOfMonth: Int, hour: Int, minute: Int, second: Int, nanoOfSecond: Int, offset: ZoneOffset) =
+    OffsetDateTime.of(year, month, dayOfMonth, hour, minute, second, nanoOfSecond, offset)
+
+  def of(date: LocalDate, time: LocalTime, offset: ZoneOffset) = OffsetDateTime.of(date, time, offset)
+
+  def of(dateTime: LocalDateTime, offset: ZoneOffset) = OffsetDateTime.of(dateTime, offset)
+
+  def ofInstant(instant: Instant, zone: ZoneId) = OffsetDateTime.ofInstant(instant, zone)
+
+  def parse(text: CharSequence) = OffsetDateTime.parse(text)
+
+  def parse(text: CharSequence, formatter: DateTimeFormatter) = OffsetDateTime.parse(text, formatter)
+
+  def timeLineOrder(): java.util.Comparator[OffsetDateTime] = OffsetDateTime.timeLineOrder()
 
 }
